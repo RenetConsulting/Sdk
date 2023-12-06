9307b962d4208be0db28a33a31391f4ce8f495b3
Alexander Altotsky <alex@renetusa.com>
2023-12-06 11:04:29 -0800

    Updated assembly and project info

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Renet Consulting, Inc.")]
 [assembly: AssemblyTrademark("™ Renet Consulting, Inc.")]
-[assembly: AssemblyCopyright("© Renet Consulting, Inc. All right reserved.")]
+[assembly: AssemblyCopyright("©2023 Renet Consulting, Inc. All right reserved.")]
 
 // This shows up as Product Version in Windows Explorer
 // We make this the same for all files in a particular product version. And increment it globally for all projects.
 // We then use this as the Product Version in installers as well (for example built using Wix).
-[assembly: AssemblyInformationalVersion("1.1.2")]
-[assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyFileVersion("1.1.2")]
+[assembly: AssemblyInformationalVersion("1.1.4")]
+[assembly: AssemblyVersion("1.1.4")]
+[assembly: AssemblyFileVersion("1.1.4")]
